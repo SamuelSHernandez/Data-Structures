713684163e72fea1b436c4f06880771fa8626069
hernsa <hernsa@cs.wallawalla.edu>
2020-04-07 04:51:19 +0000

    autosave

--- homework/00_hello_world/hello_world.cpp
+++ homework/00_hello_world/hello_world.cpp
@@ -14,6 +14,6 @@
 using namespace std;
 
 int main() {
-    cout << "hello world!" << endl;
-    return 0;
+  cout << "hello world!" << endl;
+  return 0;
 }
--- homework/01_review/manager.cpp
+++ homework/01_review/manager.cpp
@@ -3,8 +3,58 @@
  * Assignment 1: high score manager
  *
  * File Name: manager.cpp
- * Name:      ??
+ * Name:      hernsa
  * Course:    CPTR 242
- * Date:      ??
+ * Date:     4/2/20
  *
  */
+
+#include <fstream>
+#include <iomanip>
+#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+#include "manager.h"
+
+void addPlayer(int &numPlayers, Player *&players, string name, int score) {
+
+  ofstream Leaderboard_out;
+  string PlayerName;
+  int PlayerScore;
+  int minimum = 1;
+
+  Leaderboard_out.open("user_input.txt"); // open file
+  PlayerName = name;
+  if (!Leaderboard_out) {
+    cout << "cannot open file: \" user_input.txt \" " << endl;
+  }
+ minimum = (score < minimum) ? score : minimum;
+  if (numPlayers <= 10) {
+    // TO DO: Add player to file
+    for (int i = 0; i < numPlayers; i++) {
+
+      if (players[i].getName() != name && score > minimum ) {
+        players[i].setName(name);
+        Leaderboard_out << name;
+      } else if (numPlayers > 10 || numPlayers < 0) {
+        cout << "The number of players is invalid!";
+      }
+    }
+   
+   
+  }
+
+  Leaderboard_out.close();
+}
+
+// TO DO: if no score provided, score = -1
+
+// close file
+
+/*if (numPlayers > 10) {
+
+
+    */
