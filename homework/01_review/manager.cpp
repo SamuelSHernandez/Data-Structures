649095a248ccdd0a8972eeb8880026f620b29423
hernsa <hernsa@cs.wallawalla.edu>
2020-04-09 02:37:55 +0000

    autosave

--- homework/01_review/manager.cpp
+++ homework/01_review/manager.cpp
@@ -35,27 +35,28 @@ void Manager::addPlayer(int &numPlayers, Player *&players, string name,
       delete[] AddTemp;
     }
   }
+}
 
-  int Manager::searchPlayers(int numPlayers, Player *players, string target) {
+int Manager::searchPlayers(int numPlayers, Player *players, string target) {
 
-    int playerScore = 0;
-    // TO DO: Return Player's score
+  int playerScore = 0;
+  // TO DO: Return Player's score
+  for (int i = 0; i < numPlayers; i++) {
+    if (players[i].getName() == target) {
+      playerScore = players[i].getScore();
+    }
+  }
+  return playerScore;
+}
+void Manager::removePlayer(int &numPlayers, Player *&players, string target) {
+
+  if (numPlayers > 10) {
+    numPlayers -= 1;
+    Player *AddTemp = new Player[numPlayers];
     for (int i = 0; i < numPlayers; i++) {
-      if (players[i].getName() == target) {
-        playerScore = players[i].getScore();
-      }
-    }
-    return playerScore;
-  }
-  void Manager::removePlayer(int &numPlayers, Player *&players, string target) {
-
-    if (numPlayers > 10) {
-      numPlayers -= 1;
-      Player *AddTemp = new Player[numPlayers];
-      for (int i = 0; i < numPlayers; i++) {
-        AddTemp[i] = players[i];
-        players = AddTemp;
-        delete[] AddTemp;
-      }
+      AddTemp[i] = players[i];
+      players = AddTemp;
+      delete[] AddTemp;
     }
   }
+}
