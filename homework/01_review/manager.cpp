dfd5c1f68a60960d2984a9ad46a9ad1164176d25
hernsa <hernsa@cs.wallawalla.edu>
2020-04-09 01:37:55 +0000

    autosave

--- homework/01_review/manager.cpp
+++ homework/01_review/manager.cpp
@@ -22,52 +22,34 @@ using namespace std;
 void Manager::addPlayer(int &numPlayers, Player *&players, string name,
                         int score) {
 
-  ofstream Leaderboard_out;
-  string PlayerName;
-  int minimum = 1;
-  // TO DO: Delete file i/o
+  Player addPlayerArray[9];
+
+  for (int i = 0; i < numPlayers; i++) {
+
+    addPlayerArray[i] = players[i];
+  }
+
   // TO DO: Copy array into array with size + 1
   // TO DO: Sort by score
-  Leaderboard_out.open("user_input.txt"); // open file
-  PlayerName = name;
-  if (!Leaderboard_out) {
-    cout << "cannot open file: \" user_input.txt \" " << endl;
-  }
-  minimum = (score < minimum) ? score : minimum;
-
-  if (numPlayers <= 10) {
-
-    for (int i = 0; i < numPlayers; i++) {
-
-      if (players[i].getName() != name && score > minimum) {
-        players[i].setName(name);
-        Leaderboard_out << name << " " << score;
-      } else if (numPlayers > 10 || numPlayers < 0) {
-        cout << "The number of players is invalid!";
-      }
-    }
-    Player(score, name);
-  }
-
-  Leaderboard_out.close();
 }
 
 int Manager::searchPlayers(int numPlayers, Player *players, string target) {
+
+  int playerScore = 0;
   // TO DO: Return Player's score
-  int playerIndex = 0;
   for (int i = 0; i < numPlayers; i++) {
-    if (players[i].getName() == target) {
-      i = playerIndex;
-    }
+    continue;
   }
-  return playerIndex;
+  return playerScore;
 }
 void Manager::removePlayer(int &numPlayers, Player *&players, string target) {
 
-  // TO DO: Copy array into array with size - 1.  if player-> do not copy
+  Player addPlayerArray[8];
+  bool Player = false;
   for (int i = 0; i < numPlayers; i++) {
-    if (players[i].getName() == target) {
-      players[i].setName("");
-    }
+
+    addPlayerArray[i] = players[i];
+    cout << &addPlayerArray[i];
   }
+  // TO DO: Copy array into array with size - 1.  if player-> do not copy
 }
