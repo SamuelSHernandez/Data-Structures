1d93cd580d055fd1642693d4461296e1de1ae5ed
hernsa <hernsa@cs.wallawalla.edu>
2020-04-13 17:48:17 +0000

    autosave

--- homework/02_searching/LinearSearch.cpp
+++ homework/02_searching/LinearSearch.cpp
@@ -37,12 +37,12 @@ void LinearSearch::Search(vector<ItemType> list, ItemType key, int &location,
         break;
       }
 
-      if (key.ComparedTo(list[i]) == EQUAL) {
+      if (key.ComparedTo(list[i]) == EQUAL || location == -1) {
         break;
-      } else if (key.ComparedTo(list[i]) == LESS ||
+
+      } else if (!key.ComparedTo(list[i]) ||
                  key.ComparedTo(list[i]) == GREATER) {
         location = -1;
-        break;
       }
     }
   }
