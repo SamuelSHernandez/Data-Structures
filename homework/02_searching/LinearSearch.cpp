1e871ac222325414b3225b0234ac6cdf2137d798
hernsa <hernsa@cs.wallawalla.edu>
2020-04-13 18:02:26 +0000

    autosave

--- homework/02_searching/LinearSearch.cpp
+++ homework/02_searching/LinearSearch.cpp
@@ -21,26 +21,28 @@ void LinearSearch::Search(vector<ItemType> list, ItemType key, int &location,
                           int &comparisons) {
   if (list.size() != 0) {
 
-    location = -1;   // location starts at root of vector
+    location = 0;    // location starts at root of vector
     comparisons = 0; // make sure there comparison size = 0
 
     for (int i = 0; i < list.size(); i++) {
       comparisons++;
       switch (key.ComparedTo(list[i])) {
       case LESS:
-        location = 0;
+
         break;
       case GREATER:
-        location = 0;
+
         break;
       case EQUAL:
         location = i;
         break;
       }
 
-      if (key.ComparedTo(list[i]) == EQUAL || key.ComparedTo(list[0]) == LESS) {
+      if (key.ComparedTo(list[i]) == EQUAL) {
         break;
-
+      } else if (key.ComparedTo(list[0]) == LESS) {
+        break;
+        location = -1;
       } else if (!key.ComparedTo(list[i]) ||
                  key.ComparedTo(list[i]) == GREATER) {
         location = -1;
