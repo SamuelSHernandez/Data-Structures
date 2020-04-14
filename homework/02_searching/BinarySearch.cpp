33be4fdfb6207bc439a6dac82d98ceb8a7ce0c6e
hernsa <hernsa@cs.wallawalla.edu>
2020-04-14 01:23:17 +0000

    autosave

--- homework/02_searching/BinarySearch.cpp
+++ homework/02_searching/BinarySearch.cpp
@@ -24,6 +24,7 @@ void BinarySearch::Search(vector<ItemType> list, ItemType key, int &location,
     int low = 0;
     int high = list.size() - 1;
     comparisons = 0;
+    location = 0;
 
     while (low <= high) {
 
