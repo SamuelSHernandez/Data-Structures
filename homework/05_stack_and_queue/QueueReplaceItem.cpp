37155d42c2b8619c18b41716e61a7833619e1658
hernsa <hernsa@cs.wallawalla.edu>
2020-05-04 04:10:03 +0000

    autosave

--- homework/05_stack_and_queue/QueueReplaceItem.cpp
+++ homework/05_stack_and_queue/QueueReplaceItem.cpp
@@ -11,20 +11,23 @@
 
 // TODO: Implement ReplaceItem for the queue implementation
 // in QueueReplaceItem.cpp. A cpp file has been provided for you with a function
-// header.  
+// header.
 
 void QueueType::ReplaceItem(ItemType oldItem, ItemType newItem) {
   // Function: Replaces item in
-    int size = 0;
-
-    if(front < rear){
-        size = rear - front;
-    }
-
+  int size = 0;
   ItemType tempItem = items[front];
-  // TO DO: Iterate through both queues and transfer head to tail
-  for (int i = 0; i < size ; i++) {
-      Enqueue(newItem);
+  if (front < rear) {
+    size = rear - front;
   }
 
+  // TO DO: Iterate through both queues and transfer head to tail
+  for (int i = 0; i < size; i++) {
+    Dequeue(tempItem);
+    if (tempItem == oldItem) {
+      Enqueue(newItem);
+    } else {
+      Enqueue(tempItem);
+    }
+  }
 }
