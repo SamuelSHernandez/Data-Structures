070ad55d9d46f451cdbae48538031e9f168c54c5
hernsa <hernsa@cs.wallawalla.edu>
2020-05-04 17:01:31 +0000

    autosave

--- homework/05_stack_and_queue/QueueReplaceItem.cpp
+++ homework/05_stack_and_queue/QueueReplaceItem.cpp
@@ -15,24 +15,24 @@
 
 void QueueType::ReplaceItem(ItemType oldItem, ItemType newItem) {
   // Function: Replaces item in
-  int size = 0;
-  ItemType tempItem = items[front];
+  //copy the head of old to tail of new 
+QueueType tempQueue;
 
-  if (front < rear) {
-    size = rear - front;
-  }
-
-  if (front > rear) {
-     size =  maxQue - front + rear;
-  }
-
-  // TO DO: Iterate through both queues and transfer head to tail
-  for (int i = 0; i < size; i++) {
-    Dequeue(tempItem);
-    if (tempItem == oldItem) {
-      Enqueue(newItem);
-    } else {
+  while(!IsEmpty()){
+      ItemType tempItem;
+      Dequeue(tempItem);
+      if(oldItem == newItem){
+          tempItem = newItem;
+      }
       Enqueue(tempItem);
-    }
   }
+
+while(!tempQueue.IsEmpty()){
+    ItemType tempItem;
+    Dequeue(tempItem);
+    Enqueue(newItem);
+
+}
+
+
 }
