37b9c1ccf5c00fd6a6393ac25e6fb6e96605ff43
hernsa <hernsa@cs.wallawalla.edu>
2020-05-17 20:37:53 +0000

    autosave

--- homework/07_graph/DeleteEdge.cpp
+++ homework/07_graph/DeleteEdge.cpp
@@ -11,16 +11,15 @@
 #include "GraphType.h"
 
 // IMPLEMENTATION FOR DeleteEdge HERE
-
+template <class VertexType>
 // Pre:  vertex1 and vertex2 are in the set of vertices.
 // Post: (vertex1, vertex2) is not in the set of edges
-void GraphType<VertexType>::DeleteEdge(VertexType vertex_initial,
-                                       VertexType vertex_final) {
-
-  int row = IndexIs(vertex_initial);
-  int column = IndexIs(vertex_final);
+void DeleteEdge(VertexType vertex_initial, VertexType vertex_final) {
+  int row = indexIs(vertex_initial);
+  int column = indexIs(vertex_final);
 
   edges[row][column] = NULL_EDGE;
 }
+
 // Keep this at the end of the file.
 template class GraphType<int>;
--- homework/07_graph/DeleteVertex.cpp
+++ homework/07_graph/DeleteVertex.cpp
@@ -14,6 +14,32 @@
 
 // Pre: vertex1 is in the set of vertices
 // Post: vertex1 is not in the set of vertices
+template <class VertexType>
+void GraphType<VertexType>::DeleteVertex(VertexType vertex) {
+  int index = 0;
+  int index_initial = 0;
+  int index_final = 0;
+
+  // TO DO:find vertex index
+  for (int i = 0; i < numVertices; i++) {
+    if (vertices[i] == vertex) {
+      index = i;
+    }
+  }
+
+  // TO DO:delete connections
+  index_final = IndexIs(vertex);
+  for (index_initial = 0; index_initial < numVertices; index_final++) {
+    if (edges[index_initial][index_final] != NULL_EDGE) {
+      DeleteEdge(index_initial, vertices[index_initial]);
+      DeleteEdge(vertices[index_final], index_initial);
+    }
+  }
+
+  // TO DO: add to unused vertices and delete it
+  AddUnusedVertex(index);
+  vertices[index] = 0;
+}
 
 // Keep this at the end of the file.
 template class GraphType<int>;
