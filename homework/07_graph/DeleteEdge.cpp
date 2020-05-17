63e2fcb7d6c1cf3571cd86fbe0279ef9439ea160
hernsa <hernsa@cs.wallawalla.edu>
2020-05-17 20:22:07 +0000

    autosave

--- homework/07_graph/DeleteEdge.cpp
+++ homework/07_graph/DeleteEdge.cpp
@@ -3,14 +3,24 @@
  * Assignment: DeleteEdge
  *
  * File Name: DeleteEdge.cpp
- * Name:        ?
+ * Name:        Hernsa
  * Course:      CPTR 242
- * Date:        ?
+ * Date:        today
  */
 
 #include "GraphType.h"
 
 // IMPLEMENTATION FOR DeleteEdge HERE
 
+// Pre:  vertex1 and vertex2 are in the set of vertices.
+// Post: (vertex1, vertex2) is not in the set of edges
+void GraphType<VertexType>::DeleteEdge(VertexType vertex_initial,
+                                       VertexType vertex_final) {
+
+  int row = IndexIs(vertex_initial);
+  int column = IndexIs(vertex_final);
+
+  edges[row][column] = NULL_EDGE;
+}
 // Keep this at the end of the file.
 template class GraphType<int>;
--- homework/07_graph/DeleteVertex.cpp
+++ homework/07_graph/DeleteVertex.cpp
@@ -11,19 +11,9 @@
 #include "GraphType.h"
 
 // IMPLEMENTATION FOR DeleteVertex HERE
-// Pre:  vertex1 and vertex2 are in the set of vertices.
-// Post: (vertex1, vertex2) is not in the set of edges
 
 // Pre: vertex1 is in the set of vertices
 // Post: vertex1 is not in the set of vertices
-void GraphType<VertexType>::DeleteEdge(VertexType vertex_initial,
-                                       VertexType vertex_final) {
-
-  int row = IndexIs(vertex_initial);
-  int column = IndexIs(vertex_final);
-
-  edges[row][column] = NULL_EDGE;
-}
 
 // Keep this at the end of the file.
 template class GraphType<int>;
