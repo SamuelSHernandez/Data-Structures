49b98d5330f5660f5f6ab09b8e4727ab7f1643bc
hernsa <hernsa@cs.wallawalla.edu>
2020-06-03 17:28:28 +0000

    autosave

--- projects/03_usgs_binary_search_tree/UsgsTree.cpp
+++ projects/03_usgs_binary_search_tree/UsgsTree.cpp
@@ -33,7 +33,7 @@ UsgsTree::getSmallestQuake(BSTElement<float, EarthquakeUSGS> *root,
   while (current->getLeft() != NULL) {
     current = current->getLeft();
   }
-  root->NULLgetVisualizer(current)->setColor(Color("green"));
+  root->getVisualizer(current)->setColor(Color("green"));
 
   return current->getValue();
 }
